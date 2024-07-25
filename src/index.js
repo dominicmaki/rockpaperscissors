d533ff8208dac148667487c689ce85cf5007bb3c
unknown <dominic.maki@gmail.com>
2024-07-24 20:17:35 -0500

    still working on this

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+ * @file Description of what the file is for.
+ */
+
+/**
+ * This is a description of a function that is awesome for awesomeness sake.
+ * @param {number} inNum - Parameter description.
+ * @returns {number} Returns a static 3 I just typed at rando.
+ */
+
+console.log("Hello World");
